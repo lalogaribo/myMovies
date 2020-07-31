495b85d5c617cd3eff1aba978036de1118214582
Edward Mendoza <lalo.garibo90@gmail.com>
2020-07-31 09:22:53 -0700

    Added joi library to validate schemas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import "./App.css";
 
 import Movies from "./components/movies/movies";
 import Navbar from "./components/Navbar";
 import NotFound from "./components/shared/NotFound";
 import Rentals from "./components/Rentals";
 import Customers from "./components/Customers";
-import MovieDetail from "./components/movies/MovieDetail";
+import MovieForm from "./components/movies/MovieForm";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
@@ -15,12 +16,14 @@ function App() {
       <Navbar />
       <main className="container">
         <Switch>
-          <Route path="/not-found" component={NotFound} />
-          <Route path="/movies" exact component={Movies} />
-          <Route path="/movies/:id" component={MovieDetail} />
-          <Route path="/rentals" component={Rentals} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
           <Route path="/customers" component={Customers} />
-          <Route path="/" exact component={Movies} />
+          <Route path="/movies" exact component={Movies} />
+          <Route path="/movies/new" component={MovieForm} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
           <Redirect to="/not-found" />
         </Switch>
       </main>
